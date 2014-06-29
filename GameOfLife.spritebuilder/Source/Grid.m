d1c8784ceffe5ec1b092c84dcb21293549341711
ThierryN <thierrynakoa@Thierry-Nakoas-MacBook-Pro.local>
2014-06-29 02:34:27 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -101,7 +101,7 @@ static const int GRID_COLUMNS = 10;
 
 
 
--(void) countNeighbors{
+-(int) countNeighbors{
     // iterate through the rows
     // note that NSArray has a method 'count' that will return the number of elements in the array
     for (int i = 0; i < [_gridArray count]; i++)
@@ -145,9 +145,7 @@ static const int GRID_COLUMNS = 10;
         }
         
     }
-    
-
-
+    return _totalAlive;
     
 }
 
